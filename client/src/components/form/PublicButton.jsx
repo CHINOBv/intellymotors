6e8a885b9f96b10130deec222ecf0af992c3a25d
CHINOBv <retrochanel97@gmail.com>
2021-01-03 20:48:11 -0600

    Manejo de error en caso de desconeccion con servidor

--- client/src/components/form/PublicButton.jsx
+++ client/src/components/form/PublicButton.jsx
@@ -40,8 +40,17 @@ const PublicButton = () => {
   const fetchAPI = async (e) => {
     e.preventDefault();
 
+    if (showAlert) {
+      setShowAlert(false);
+      setAlertInfo({
+        message: "",
+        typeAlert: "",
+        title: "",
+      });
+    }
+
     //Validate if all fields are filled
-    if (price <= 0 && description.trim() === "") {
+    if (price <= 0 || description.trim() === "") {
       setShowAlert(true);
       setAlertInfo({
         message: "Todos los campos son Requeridos",
@@ -59,55 +68,38 @@ const PublicButton = () => {
       return;
     }
 
-    if (showAlert) {
-      setShowAlert(false);
-      setAlertInfo({
-        message: "",
-        typeAlert: "",
-        title: "",
-      });
-    }
-
     setIsLoading(true);
-    const response = await publicNewCar(price, description);
+    await publicNewCar(price, description)
+      .then((response) => {
+        setImage(response);
 
-    //Case of Error
-    if (!response) {
-      setShowAlert(true);
-      setAlertInfo({
-        message: "Hemos tenido un error, porfavor intentalo otra vez",
-        typeAlert: "error",
-        title: "Error",
-      });
-      setTimeout(() => {
-        setShowAlert(false);
+        setShowAlert(true);
         setAlertInfo({
-          message: "",
-          typeAlert: "",
-          title: "",
+          message: "Anuncio Publicado!",
+          typeAlert: "success",
+          title: "Ready",
         });
-      }, 5000);
-      return;
-    }
-
-    console.log(response)
-    setImage(response);
-    setIsLoading(false);
-    setShowAlert(true);
-    setAlertInfo({
-      message: "Anuncio Publicado!",
-      typeAlert: "success",
-      title: "Ready",
-    });
-
-    setTimeout(() => {
-      setShowAlert(false);
-      setAlertInfo({
-        message: "",
-        typeAlert: "",
-        title: "",
+      })
+      .catch((error) => {
+        //Error Case
+        setShowAlert(true);
+        setAlertInfo({
+          message: `Hemos tenido un error, porfavor intentalo otra vez ${error}`,
+          typeAlert: "error",
+          title: "Error",
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setTimeout(() => {
+          setShowAlert(false);
+          setAlertInfo({
+            message: "",
+            typeAlert: "",
+            title: "",
+          });
+        }, 5000);
       });
-    }, 5000);
   };
 
   return (
--- client/src/utils/fetchAPI.js
+++ client/src/utils/fetchAPI.js
@@ -3,15 +3,17 @@ import axios from "axios";
 let URL = "http://localhost:4000";
 
 export const publicNewCar = async (priceReq, descriptionReq) => {
-  let response = await axios.post(`${URL}/vehicle`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
+  return await axios
+    .post(`${URL}/vehicle`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-    price: priceReq,
-    description: descriptionReq,
-  });
-  const { image, price, description } = await response.data;
-
-  return { image: URL + image, price, description };
+      price: priceReq,
+      description: descriptionReq,
+    })
+    .then((response) => {
+      const { image, price, description } = response.data;
+      return { image: URL + image, price, description };
+    }).catch(err => Promise.reject(err))
 };
