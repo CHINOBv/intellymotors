c24e7b6714875acf9c1101120e1e031709b2af3d
CHINOBv <retrochanel97@gmail.com>
2021-01-03 23:20:52 -0600

    sin efecto de guardado

--- client/src/components/ImageFullView.jsx
+++ client/src/components/ImageFullView.jsx
@@ -11,28 +11,6 @@ const ImageFullView = ({ setOpenImage, openImage, image }) => {
     setOpenImage(false);
   };
 
-  const download = (e) => {
-    //    e.preventDefault();
-    console.log(e.target.href);
-    fetch(e.target.href, {
-      method: "GET",
-      headers: {},
-    })
-      .then((response) => {
-        response.arrayBuffer().then(function (buffer) {
-          const url = window.URL.createObjectURL(new Blob([buffer]));
-          const link = document.createElement("a");
-          link.href = url;
-          link.setAttribute("download", "image.jpg"); //or any other extension
-          document.body.appendChild(link);
-          link.click();
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   return (
     <Dialog
       fullScreen
@@ -43,8 +21,6 @@ const ImageFullView = ({ setOpenImage, openImage, image }) => {
       <DialogContent>
         <a
           href={image}
-          download
-          onClick={(e) => download(e)}
           target="_blank"
           rel="noreferrer"
         >
